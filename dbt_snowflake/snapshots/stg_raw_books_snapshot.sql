6eb271e7d3b4d6bffe897ffad74d946cba3c0c3b
Caio Oliveira <caio.oliveira@qualityeng.com.br>
2025-01-15 19:19:16 -0300

    finished with snapshots

--- dbt_snowflake/snapshots/stg_raw_books_snapshot.sql
+++ dbt_snowflake/snapshots/stg_raw_books_snapshot.sql
@@ -1,18 +1,17 @@
 {% snapshot stg_raw_books_snapshot %}
 
-{{ config(
-    target_database='DBT_PROJECT',
-    target_schema='snapshots',
-    unique_key='unique_id', -- Use the generated unique key
-    strategy='timestamp',
-    updated_at='updated_at',
-    check_cols=['*']
-) }}
+    {{
+        config(
+            target_schema='snapshots',
+            unique_key='unique_id',
+            strategy='check',
+            check_cols=['*']
+        )
+    }}
 
-SELECT
-    MD5(CONCAT(sales_year, '-', Position)) AS unique_id,
-    *
-FROM {{ source('dbt_staging', 'stg_raw_books') }}
-
+        SELECT
+            HASH(sales_year || '-' || Position) AS unique_id, -- Use Snowflake-compatible hash function
+            *
+        FROM {{ source('dbt_staging', 'stg_raw_books') }}
 
 {% endsnapshot %}
